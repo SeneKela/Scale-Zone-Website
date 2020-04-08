3020592e1cc051a682c1a0dc3c1db57d98ef44b2
SeneKela <62216296+SeneKela@users.noreply.github.com>
2020-04-08 12:33:52 +0200

    Maj4

--- js/main.js
+++ js/main.js
@@ -11,7 +11,7 @@
 
   // loader
   var loader = function() {
-    setTimeout(function() { 
+    setTimeout(function() {
       if($('#loader').length > 0) {
         $('#loader').removeClass('show');
       }
@@ -19,7 +19,7 @@
   };
   loader();
 
-	
+
 	$('nav .dropdown').hover(function(){
 		var $this = $(this);
 		$this.addClass('show');
@@ -39,8 +39,8 @@
 	$('#dropdown04').on('show.bs.dropdown', function () {
 	  console.log('show');
   });
-  
-  
+
+
 	// home slider
 	$('.home-slider').owlCarousel({
     loop:true,
@@ -172,7 +172,7 @@
 		$('.element-animate').waypoint( function( direction ) {
 
 			if( direction === 'down' && !$(this.element).hasClass('element-animated') ) {
-				
+
 				i++;
 
 				$(this.element).addClass('item-animate');
@@ -194,9 +194,9 @@
 							el.removeClass('item-animate');
 						},  k * 100);
 					});
-					
+
 				}, 100);
-				
+
 			}
 
 		} , { offset: '95%' } );
@@ -215,7 +215,43 @@
     });
   };
   slideImage();
-  
-  
+
+  var formatThousandsNoRounding = function(n, dp){
+    var e = '', s = e+n, l = s.length, b = n < 0 ? 1 : 0,
+        i = s.lastIndexOf(','), j = i == -1 ? l : i,
+        r = e, d = s.substr(j+1, dp);
+    while ( (j-=3) > b ) { r = '.' + s.substr(j, 3) + r; }
+    return s.substr(0, j + 3) + r +
+      (dp ? ',' + d + ( d.length < dp ?
+          ('00000').substr(0, dp - d.length):e):e);
+  };
+formatThousandsNoRounding();
+
+  var hasRun = false;
+
+  inView('#countUp').on('enter', function() {
+      if (hasRun == false) {
+          $('.number').each(function() {
+              var $this = $(this),
+                  countTo = $this.attr('data-count');
+
+              $({ countNum: $this.text()}).animate({
+                  countNum: countTo
+              },
+              {
+                  duration: 2000,
+                  easing:'linear',
+                  step: function() {
+                      $this.text(formatThousandsNoRounding(Math.floor(this.countNum)));
+                  },
+                  complete: function() {
+                      $this.text(formatThousandsNoRounding(this.countNum));
+                  }
+              });
+          });
+          hasRun = true;
+      }
+  });
+inView();
 
 })(jQuery);
