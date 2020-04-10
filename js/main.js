0e96384d841a1847e743c0842f3dfa3d0bd3016e
SeneKela <62216296+SeneKela@users.noreply.github.com>
2020-04-10 10:47:45 +0200

    Maj100401

--- js/main.js
+++ js/main.js
@@ -254,4 +254,14 @@ formatThousandsNoRounding();
   });
 inView();
 
+
+function copyToClipboard(element) {
+var $temp = $("<input>");
+$("body").append($temp);
+$temp.val($(element).text()).select();
+document.execCommand("copy");
+$temp.remove();
+}
+copyToClipboard();
+
 })(jQuery);
